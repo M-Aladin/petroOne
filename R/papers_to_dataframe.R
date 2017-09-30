f728292609996cc4c67eb7f2ea689eaa636c5c10
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-09-30 17:27:35 -0500

    movinf functions to papers_to_dataframe

---
+++ R/papers_to_dataframe.R
@@ -0,0 +1,105 @@
+
+
+
+onepetro_page_to_dataframe <- function(url) {
+    webpage <- read_html(url)
+    df_titles  <- read_titles(webpage)
+    df_sources <- read_sources(webpage)
+    df_author  <- read_author(webpage)
+
+    # ensure that all dataframe have the same number of rows
+    if (all(dim(df_titles)[1]  == dim(df_sources)[1],
+            dim(df_sources)[1] == dim(df_author)[1],
+            dim(df_author)[1]  == dim(df_titles)[1]
+    ))
+        cbind(df_titles, df_sources, df_author)
+    else
+        stop("Dataframe sizes different")  # otherwise, stop
+}
+
+
+
+
+
+onepetro_allpages_to_dataframe <- function(url) {
+    # webpage <- read_html(url)
+    papers_count <- get_papers_count(url)
+    if (papers_count > 1000) {
+        num_pages <- papers_count / 1000
+    } else {
+        num_pages = 1
+    }
+
+    info <- list(papers = papers_count, pages1000 = num_pages)
+
+    for (page in seq_len(num_pages)) {
+        # webpage <- read_html(url)
+
+    }
+    info
+}
+
+
+#' @importFrom rvest html_nodes html_text
+read_titles <- function(webpage) {
+
+    # title
+    # .result-link
+
+    #Using CSS selectors to scrap the rankings section
+    title_data_html <- html_nodes(webpage, '.result-link')
+
+    # Converting the ranking data to text
+    title_data <- html_text(title_data_html)
+
+    # data pre-processing
+    title_data <- trimws(gsub("\n", "",title_data))
+
+
+    #Let's have a look at the rankings
+    title_data <- data.frame(title_data, stringsAsFactors = FALSE)
+    # write.csv(df, file = "3000-conf.csv")
+    return(title_data)
+}
+
+#' @importFrom rvest html_nodes html_text
+read_sources <- function(webpage) {
+    # year, paper id, institution, type, year
+    # .result-item-source
+
+    #Using CSS selectors to scrap the rankings section
+    source_data_html <- html_nodes(webpage, '.result-item-source')
+
+    #Converting the ranking data to text
+    source_data <- html_text(source_data_html)
+
+    # pre-processing. split at \n
+    source_data <- data.frame(do.call('rbind', strsplit(as.character(source_data),
+                                                        '\n',fixed=TRUE)),
+                              stringsAsFactors = FALSE)
+    # remove blank columns
+    source_data <- source_data[, 2:5]
+    # rename columns
+    names(source_data) <- c("paper_id", "source", "type", "year")
+    # remove dash from year
+    source_data$year <- gsub("-", "", source_data$year)
+
+    # Let's have a look at the paper source data
+    source_data
+}
+
+#' @importFrom rvest html_nodes html_text
+read_author <- function(webpage) {
+    # author #1
+    #Using CSS selectors to scrap the rankings section
+    author1_data_html <- html_nodes(webpage, '.result-item-author:nth-child(1)')
+
+    #Converting the ranking data to text
+    author1_data <- html_text(author1_data_html)
+
+    # data pre-processing
+    author1_data <- trimws(gsub("\n", "", author1_data))
+
+    #Let's have a look at the rankings
+    data.frame(author1_data, stringsAsFactors = FALSE)
+}
--- R/url.R
+++ R/url.R
@@ -17,10 +17,7 @@ form_input <- list(dummy = "dummy", query = "?q=", peer_reviewed = "peer_reviewe
 # https://www.onepetro.org/search?q=%22data+science%22&peer_reviewed=&published_between=&from_year=&to_year=
 #
 
-send_url <- function(url, how = "any") {
-  #Reading the HTML code from the website
-  read_html(url)
-}
+
 
 
 #' @title Read OnePetro web page given a query URL
@@ -177,108 +174,8 @@ make_search_url <- function(query = NULL, start = NULL, from_year = NULL,
 }
 
 
-#' @importFrom rvest html_nodes html_text
-read_titles <- function(webpage) {
-
-    # title
-    # .result-link
-
-    #Using CSS selectors to scrap the rankings section
-    title_data_html <- html_nodes(webpage, '.result-link')
-
-    # Converting the ranking data to text
-    title_data <- html_text(title_data_html)
-
-    # data pre-processing
-    title_data <- trimws(gsub("\n", "",title_data))
 
 
-    #Let's have a look at the rankings
-    title_data <- data.frame(title_data, stringsAsFactors = FALSE)
-    # write.csv(df, file = "3000-conf.csv")
-    return(title_data)
-}
-
-#' @importFrom rvest html_nodes html_text
-read_sources <- function(webpage) {
-    # year, paper id, institution, type, year
-    # .result-item-source
-
-    #Using CSS selectors to scrap the rankings section
-    source_data_html <- html_nodes(webpage, '.result-item-source')
-
-    #Converting the ranking data to text
-    source_data <- html_text(source_data_html)
-
-    # pre-processing. split at \n
-    source_data <- data.frame(do.call('rbind', strsplit(as.character(source_data),
-                                                        '\n',fixed=TRUE)),
-                              stringsAsFactors = FALSE)
-    # remove blank columns
-    source_data <- source_data[, 2:5]
-    # rename columns
-    names(source_data) <- c("paper_id", "source", "type", "year")
-    # remove dash from year
-    source_data$year <- gsub("-", "", source_data$year)
-
-    # Let's have a look at the paper source data
-    source_data
-}
-
-#' @importFrom rvest html_nodes html_text
-read_author <- function(webpage) {
-    # author #1
-    #Using CSS selectors to scrap the rankings section
-    author1_data_html <- html_nodes(webpage, '.result-item-author:nth-child(1)')
-
-    #Converting the ranking data to text
-    author1_data <- html_text(author1_data_html)
-
-    # data pre-processing
-    author1_data <- trimws(gsub("\n", "", author1_data))
-
-    #Let's have a look at the rankings
-    data.frame(author1_data, stringsAsFactors = FALSE)
-}
-
-
-onepetro_page_to_dataframe <- function(url) {
-    webpage <- read_html(url)
-    df_titles  <- read_titles(webpage)
-    df_sources <- read_sources(webpage)
-    df_author  <- read_author(webpage)
-
-    # ensure that all dataframe have the same number of rows
-    if (all(dim(df_titles)[1]  == dim(df_sources)[1],
-            dim(df_sources)[1] == dim(df_author)[1],
-            dim(df_author)[1]  == dim(df_titles)[1]
-                                  ))
-        cbind(df_titles, df_sources, df_author)
-    else
-        stop("Dataframe sizes different")  # otherwise, stop
-}
-
-
-
-
-
-onepetro_allpages_to_dataframe <- function(url) {
-    # webpage <- read_html(url)
-    papers_count <- get_papers_count(url)
-    if (papers_count > 1000) {
-        num_pages <- papers_count / 1000
-    } else {
-        num_pages = 1
-    }
-
-    info <- list(papers = papers_count, pages1000 = num_pages)
-
-    for (page in seq_len(num_pages)) {
-        # webpage <- read_html(url)
-
-    }
-    info
-}
 
 
 
@@ -338,4 +235,10 @@ create_url <- function(start = NULL, query = NULL, from_year = NULL,
     url <- paste(s_q, s_peer, s_publish, s_from, s_to, sep = "&")
     url <- paste0(s_search, url)
     url
+}
+
+
+send_url <- function(url, how = "any") {
+    #Reading the HTML code from the website
+    read_html(url)
 }
